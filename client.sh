7d715a41a5835288ee6d56ff5b2d6504a5818562
stefanos1316 <stefanos1316@gmail.com>
2020-05-15 15:07:37 +0300

    Add complete client program

--- client.sh
+++ client.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+if [ "$1" == "-h" -o "$1" == "--help" ]; then
+	echo
+	echo A simple client program to managa the server functions
+	echo ======================================================
+	echo
+	echo To add user use -a or --addUser with the second command line argument as 
+	echo '"name":"Istvan","surname":"Tarnok","email":"sgeorgiou@aueb.gr","password":"somethingHungarian"'
+	echo
+	echo To get user use -g or --getUser with the second command line argument as
+	echo '"email":"stefanos1316@gmail.com"'
+	echo
+	echo To compress an image use -c or --compress and as a second command line argument
+	echo pass the url from an online image
+	echo
+	exit
+fi
+
 case $1 in
 	-c|--compress) 
 		curl --request POST --url "http://localhost:3000/compress" \
